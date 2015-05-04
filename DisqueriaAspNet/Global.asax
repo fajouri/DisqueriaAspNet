f1143f44616eeb2a796e0e557a8f9fd4abaa05fb
yfajouri <yef03@hotmail.com>
2015-05-04 10:54:09 -0300

    #0

---
+++ DisqueriaAspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DisqueriaAspNet.Global" Language="C#" %>
---
+++ DisqueriaAspNet/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DisqueriaAspNet.Startup))]
+namespace DisqueriaAspNet
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
